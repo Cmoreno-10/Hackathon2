9571808604f977a8e48e329aacb8b953c83481d4
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2024-02-08 11:52:15 -0500

    feat: se agrega la clase contacto

--- AgendaTelefonica/src/Contacto.java
+++ AgendaTelefonica/src/Contacto.java
@@ -1,2 +1,36 @@
 public class Contacto {
+
+    private String nombre;
+    private long numero;
+    private int idContacto;
+    private static int contadorContactos;
+
+    public Contacto (String nombre, long numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+        Contacto.contadorContactos++;
+        this.idContacto = contadorContactos;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public void mostrarContacto(){
+        System.out.println("Id: "+ idContacto + ", Nombre: " + this.nombre + ", Numero: " + this.numero);
+    }
+
 }
