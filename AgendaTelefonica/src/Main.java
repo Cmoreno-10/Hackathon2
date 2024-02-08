7fb2ab2135b5cc68fdb557b4a515be9f49320ab0
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2024-02-08 11:49:49 -0500

    feat: se agregan opciones de agregar contacto, listar contactos, revisar si la agenda esta llena y validar el espacio de la agenda

--- AgendaTelefonica/src/Main.java
+++ AgendaTelefonica/src/Main.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Contacto> agenda = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int opcion;
+        int longitudAgenda = 10;
+        long numeroAgenda;
+        String nombreAgenda;
 
         // Recorrido del menú
         do {
@@ -25,10 +30,57 @@ public class Main {
 
             // Condicionar el ingreso del usuario
             switch (opcion) {
+                case 1:
+                    // Mensaje de seleccion de usuario
+                    System.out.println("==== HA SELECCIONADO AGREGAR CONTACTO ====");
+
+                    // Condicionar el agregar o no un usuario
+                    if (agenda.size() < longitudAgenda) {
+                        // Solicitud de numero a usuario
+                        System.out.println("Ingrese el numero: ");
+                        numeroAgenda = scanner.nextLong();
+
+                        // Solicitud de nombre a usuario
+                        System.out.println("Ingrese el nombre: ");
+                        nombreAgenda = scanner.next();
+
+                        // Agregar a array el contacto
+                        agenda.add(new Contacto(nombreAgenda, numeroAgenda));
+                        // Confirmar que el contacto ha sido agregado
+                        System.out.println("El usuario " + nombreAgenda + "con numero de telefono " + numeroAgenda + " ha sido agregado.");
+                    } else {
+                        System.out.println("La agenda se encuentra completa. Elimine un contacto como alternativa.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("==== HA SELECCIONADO MOSTRAR AGENDA ====");
+                    mostrarAgenda(agenda);
+                    break;
+                case 6:
+                    System.out.println("==== HA SELECCIONADO SABER SI LA AGENDA ESTA LLENA ====");
+                    if (agenda.size() == longitudAgenda) {
+                        System.out.println("La agenda se encuentra llena. Por favor elimine algún contacto de ser necesario.");
+                    } else {
+                        System.out.println("La agenda aún no se encuentra llena. Puede agregar más contactos.");
+                    }
+                    break;
+                case 7:
+                    System.out.println("==== HA SELECCIONADO SABER EL ESPACIO DISPONIBLE ====");
+                    int espacioDisponible = longitudAgenda - agenda.size();
+                    System.out.println("Tiene espacio para agregar [" + espacioDisponible + "] contacto.");
+                    break;
                 case 8:
                     System.out.println("Gracias por visitar tu agenda telefonica. Adiós.");
             }
 
         } while (opcion != 8);
+    }
+
+    // Método para mostrar la información de todos los contactos en la agenda
+    public static void mostrarAgenda(ArrayList<Contacto> agenda) {
+        System.out.println("Agenda Telefónica:");
+        for (Contacto contacto : agenda) {
+            contacto.mostrarContacto();
+        }
     }
 }
