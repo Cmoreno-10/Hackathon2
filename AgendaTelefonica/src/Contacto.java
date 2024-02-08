54d9df5ec16ffa884482fdec07851a7576446234
Julian <julianricardo423@gmail.com>
2024-02-08 10:49:44 -0600

    Se modifica el switch de opciones agregando opciones Eliminar y Listar contactos, además en la clase Contacto se crean los metodos listarContactos y eliminarContactos

--- AgendaTelefonica/src/Contacto.java
+++ AgendaTelefonica/src/Contacto.java
@@ -1,2 +1,94 @@
-public class Contacto {
+import java.util.ArrayList;
+
+public class Contacto
+{
+    /*Atributos de la clas*/
+    private String nombre;
+    private String apellido;
+    private String celular;
+
+    /*Lista tipo contactos*/
+    private ArrayList<Contacto> listaContactos = new ArrayList<>(10);
+
+
+    /**
+     * Constructor por defecto para poder acceder a los métodos de la clase, más no asignar
+     * valores a la instacia
+     */
+    public Contacto()
+    {
+
+    }
+
+    /**
+     * Constructor con los atributos para poder crear el objeto tipo contacto
+     * @param nombre
+     * @param apellido
+     * @param celular
+     */
+    public Contacto(String nombre, String apellido, String celular)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celular = celular;
+    }
+
+    /*Getters y Setters*/
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void agregarLista(Contacto contacto)
+    {
+        listaContactos.add(contacto);
+    }
+
+
+    /**
+     * Método para devolver la lista actual de los contactos guardados
+     */
+    public void listarContactos()
+    {
+        for (Contacto x: listaContactos)
+        {
+            System.out.println(x.nombre + " " + x.apellido + " " + x.celular);
+        }
+    }
+
+    /**
+     * @param c Es la cadena completa del contacto que se debe buscar para obtener el index
+     *          y al final de la búsqueda borrarlo de la lista
+     */
+    public void eliminarContacto(Contacto c)
+    {
+        System.out.println("Ingrese");
+        int index = 0;
+        for (int i = 0; i < listaContactos.size(); i++) {
+            if(String.valueOf(listaContactos.get(i).nombre).equals(c.nombre))
+            {
+                index = i;
+            }
+        }
+        listaContactos.remove(index);
+    }
 }
--- AgendaTelefonica/src/Main.java
+++ AgendaTelefonica/src/Main.java
@@ -1,32 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        Contacto contactoAux = new Contacto();
+
         int opcion;
 
         // Recorrido del menú
         do {
             // Listar el menu
-            System.out.println("==== Menú =====");
-            System.out.println("1. Agregar contacto");
-            System.out.println("2. Verificar existencia de contacto");
-            System.out.println("3. Listar contactos");
-            System.out.println("4. Buscar contacto");
-            System.out.println("5. Eliminar contacto");
-            System.out.println("6. Validar si la agenda esta llena");
-            System.out.println("7. Validar espacio de agenda");
-            System.out.println("8. Salir\n");
+            System.out.println("==== Menú =====\n" +
+                    "1. Agregar contacto\n" +
+                    "2. Verificar existencia de contacto\n" +
+                    "3. Listar contactos\n" +
+                    "4. Buscar contacto\n" +
+                    "5. Eliminar contacto\n" +
+                    "6. Validar si la agenda esta llena\n" +
+                    "7. Validar espacio de agenda\n" +
+                    "8. Salir\n");
 
             // Solicitar opción de usuario
             System.out.print("Seleccione una de las opciones previas: ");
             opcion = scanner.nextInt();
 
             // Condicionar el ingreso del usuario
-            switch (opcion) {
+            switch (opcion)
+            {
+                case 3:
+                    contactoAux.listarContactos();
+                    break;
+                case 5:
+                    //Cambiar el parametro por dato buscado
+                    contactoAux.eliminarContacto(contactoAux);
                 case 8:
                     System.out.println("Gracias por visitar tu agenda telefonica. Adiós.");
+                    break;
             }
 
         } while (opcion != 8);
